80917f031db322da9dcbf2cfaf25224a11109aee
mdshi5 <mdshi50101@gmail.com>
2024-08-25 11:22:35 +0600

    add sonar-scanner runner new changes

--- sonarqube-scan.sh
+++ sonarqube-scan.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Define API endpoint URL
-API_URL="http://aescontroller-sonarqube-sonarqube.core-ns:9000/sonarqube/api/ce/component?component=${SONAR_PROJECT_KEY}&branch=${GIT_BRANCH}"
+SONAR_PROJECT_KEY="code"
+GIT_BRANCH="main"
+API_URL="http://172.18.255.200:9000/sonarqube/api/ce/component?component=${SONAR_PROJECT_KEY}&branch=${GIT_BRANCH}"
+SONAR_TOKEN="squ_d918cdb2cc2143427334155c177dc15d930ce10e"
+
 # Polling interval in seconds
 interval=10
-TARGET_DURATION=3600
 
 while true; do
     # Make a GET request to the API endpoint
@@ -23,13 +26,8 @@ while true; do
         echo "Current status is: $current_status"
     fi
 
-    if [ "$SECONDS" -ge "$TARGET_DURATION" ]; then
-        echo "2 hours have passed. Terminating..."
-        break
-    fi
-
     # Wait for the defined interval before polling again
     sleep $interval
 done
 
-# echo "Polling completed."
+echo "Polling completed."
